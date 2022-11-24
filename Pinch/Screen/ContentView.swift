f2d0644f81f321e2e35a547f5051184373b29408
joyson.ps <joyson.ps@helpshift.com>
2022-11-24 17:22:41 +0530

    controls added to zoom in,out and reset

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,9 +11,20 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    
+    //MARK: - RESET FUNCTION
+    func resetImageState(){
+        return  withAnimation(.spring()){
+            imageOffset = .zero
+            imageScale = 1
+        }
+    }
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -21,6 +32,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2) {
                         if (imageScale == 1){
@@ -29,19 +41,87 @@ struct ContentView: View {
                             }
                         }else{
                             withAnimation(.spring()) {
-                                imageScale = 1
+                                resetImageState()
                             }
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if (imageScale <= 1){
+                                    withAnimation(.spring()){
+                                        resetImageState()
+                                    }
+                                }
+                            })
+                    )
                 
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
-            .onAppear {
+            .onAppear(perform: {
                 withAnimation(.linear(duration: 1)) {
                     isAnimating = true
                 }
-            }
+            })
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top,30)
+                
+                ,alignment: .top
+            )
+            
+            .overlay(
+                Group {
+                    HStack {
+                        Button {
+                            withAnimation(.spring()){
+                                if (imageScale > 1){
+                                    imageScale -= 1
+                                    
+                                    if (imageScale <= 1){
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        Button {
+                            withAnimation(.spring()){
+                                if (imageScale < 5){
+                                    imageScale += 1
+                                    
+                                    if (imageScale > 5){
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    }
+                    .padding(EdgeInsets(top: 12, leading: 29, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                }
+                    .padding()
+                ,alignment: .bottom
+            )
+            
         }
         .navigationViewStyle(.stack)
     }
@@ -50,6 +130,9 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
+            .previewLayout(.device)
+            .previewDevice("iPhone 12 Pro")
             .preferredColorScheme(.dark)
     }
 }
