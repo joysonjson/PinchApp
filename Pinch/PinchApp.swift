b314eb3792ca1ae4a87e92f25a43d5fd1fa2ce4a
joyson.ps <joyson.ps@helpshift.com>
2022-11-24 15:20:05 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Joyson P S on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
